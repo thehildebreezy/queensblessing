a63f75ee8e604d4bdcfd03c55d892ce69c7f1ca2
Tanner Hildebrand <tanner.hildebrand@gmail.com>
2020-09-26 10:53:13 -0400

    Framework layout completed

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      devTools: true
     }
   })
 
   // and load the index.html of the app.
   win.loadFile('main.html')
+  //win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
